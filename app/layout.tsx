1b7ab4161b2e0c8ee1433a9c6fb1657f51ef9c00
juliocesarblancomedina08-tech <juliocesarblancomedina08@gmail.com>
2026-09-01 16:17:33 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,26 @@
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
-export const metadata = {
-  title: "Cuban Miner",
-  description: "Mining Game",
+export const metadata: Metadata = {
+  title: "🇨🇺 CUBAN-MINER ⛏️",
+  description: "🇨🇺 CUBAN-MINER ⛏️ - Mini App de Telegram",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body>{children}</body>
     </html>
   );
