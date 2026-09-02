ebe2e3c08fec0784f717bbed14a5a799fde5dac9
juliocesarblancomedina08-tech <juliocesarblancomedina08@gmail.com>
2026-09-02 15:10:32 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,170 +7,67 @@ export default function Home() {
 
   if (started) {
     return (
-      <main className="h-screen bg-black flex items-center justify-center text-white text-4xl">
-        GAME STARTED
+      <main className="game-screen">
+        <h1>GAME STARTED</h1>
       </main>
     );
   }
 
   return (
-    <main className="h-screen overflow-hidden bg-gradient-to-b from-stone-900 via-black to-stone-800 relative">
+    <main className="welcome-screen">
 
       {/* partículas */}
-      <div className="absolute w-3 h-3 bg-yellow-400 rounded-full top-20 left-10 animate-ping" />
-      <div className="absolute w-2 h-2 bg-yellow-300 rounded-full top-40 right-20 animate-pulse" />
-      <div className="absolute w-4 h-4 bg-yellow-500 rounded-full bottom-40 left-32 animate-bounce" />
+      <div className="particle p1"></div>
+      <div className="particle p2"></div>
+      <div className="particle p3"></div>
+      <div className="particle p4"></div>
 
       {/* titulo */}
-      <h1
-        className="
-        absolute
-        top-16
-        w-full
-        text-center
-        text-5xl
-        font-bold
-        text-yellow-400
-        drop-shadow-lg
-        "
-      >
-        CUBAN MINER
+      <h1 className="game-title">
+        ⛏️ CUBAN MINER
       </h1>
 
-      {/* mina */}
-      <div
-        className="
-        absolute
-        bottom-0
-        w-full
-        h-56
-        bg-stone-900
-        border-t-8
-        border-stone-700
-        "
-      />
-
       {/* minero */}
-      <div
-        className="
-        absolute
-        left-1/2
-        top-1/2
-        -translate-x-1/2
-        -translate-y-1/2
-        "
-      >
-        {/* casco */}
-        <div
-          className="
-          w-24
-          h-10
-          bg-yellow-500
-          rounded-t-full
-          mx-auto
-          "
-        />
+      <div className="miner-container">
 
-        {/* cabeza */}
-        <div
-          className="
-          w-20
-          h-20
-          bg-orange-200
-          rounded-full
-          mx-auto
-          "
-        />
+        <div className="helmet"></div>
 
-        {/* cuerpo */}
-        <div
-          className="
-          w-24
-          h-32
-          bg-blue-600
-          rounded-lg
-          mx-auto
-          mt-1
-          "
-        />
+        <div className="head">
+          <div className="eyes">
+            <span></span>
+            <span></span>
+          </div>
+        </div>
 
-        {/* brazo sosteniendo pico */}
-        <div
-          className="
-          absolute
-          left-[-30px]
-          top-[95px]
-          w-16
-          h-4
-          bg-orange-200
-          rotate-[-30deg]
-          "
-        />
+        <div className="body"></div>
 
-        {/* pico */}
-        <div
-          className="
-          absolute
-          left-[-70px]
-          top-[55px]
-          text-5xl
-          "
-        >
+        <div className="left-arm"></div>
+
+        <div className="pickaxe">
           ⛏
         </div>
 
-        {/* brazo saludando */}
-        <div
-          className="
-          absolute
-          right-[-20px]
-          top-[90px]
-          w-16
-          h-4
-          bg-orange-200
-          origin-left
-          animate-[wave_1s_ease-in-out_infinite]
-          "
-        />
+        <div className="right-arm"></div>
+
+        <div className="left-leg"></div>
+
+        <div className="right-leg"></div>
+
       </div>
 
-      {/* boton */}
-      <div className="absolute bottom-24 w-full flex justify-center">
-        <button
-          onClick={() => setStarted(true)}
-          className="
-          bg-yellow-400
-          text-black
-          text-2xl
-          font-bold
-          px-12
-          py-5
-          rounded-2xl
-          shadow-yellow-500
-          shadow-lg
-          hover:scale-110
-          transition
-          "
-        >
-          START MINING
-        </button>
-      </div>
+      <p className="welcome-text">
+        Welcome Miner
+      </p>
 
-      <style jsx>{`
-        @keyframes wave {
-          0% {
-            transform: rotate(20deg);
-          }
+      <button
+        className="start-button"
+        onClick={() => setStarted(true)}
+      >
+        START MINING
+      </button>
 
-          50% {
-            transform: rotate(-30deg);
-          }
-
-          100% {
-            transform: rotate(20deg);
-          }
-        }
-      `}</style>
+      {/* suelo */}
+      <div className="mine-ground"></div>
     </main>
   );
-          }
+}
